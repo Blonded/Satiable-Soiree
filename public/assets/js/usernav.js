f8f13a487d5fbf962d8cc6be7de465e23dea0cc5
Jessica Brush <jessicadawnbrush@gmail.com>
2018-06-14 10:21:56 -0700

    usernav

--- public/assets/js/usernav.js
+++ public/assets/js/usernav.js
@@ -26,27 +26,28 @@ function getOccasions(userId) {
 
         }
         
-        var block = "";
-        block += "<div class='col-4'>";
-          block += "<div class='card' style='margin-top:25px;'>";
-            block +="<img class='card-img-top img-fluid' src='http://visitlawrencecounty.com/wp-content/uploads/2017/11/picnic.jpg' alt='Card image cap'>";
-            block +="<div class='card-body'>"              
-              block +="<h5 class='card-title'>"+name+"</h5>";
-              block +="<ul>";
-                if(posts[index].number == 0) {
-                  block +="<li>Address: "+posts[index].street+", "+posts[index].zipcode+"</li>";
-                } else {
-                  block +="<li>Address: "+posts[index].number+" "+posts[index].street+", "+posts[index].zipcode+"</li>";
-                }
-                block +="<li>Address: "+posts[index].city+"</li>";
-                block +="<li>"+posts[index].city+"</li>";
-                block +="<li>"+posts[index].city+"</li>";
-              block +="</ul>";
-              block += "<a href='#' class='btn btn-primary'>More details</a>";
-            block +="</div>";            
+          var block = "";
+          block += "<div class='col-4'>";
+            block += "<div class='card' style='margin-top:25px;'>";
+              block +="<img class='card-img-top img-fluid' src='http://visitlawrencecounty.com/wp-content/uploads/2017/11/picnic.jpg' alt='Card image cap'>";
+              block +="<div class='card-body'>";              
+                block +="<h5 class='card-title'>"+name+"</h5>";
+                block +="<ul>";
+                  block += "<li>" + posts[index].name + "</li>";
+                  block += "<li>Date: " + posts[index].date + "</li>";
+                  block += "<li>Start Time: " + posts[index].starttime + "</li>";
+                  block += "<li>End Time: " + posts[index].endtime + "</li>";
+                  if(posts[index].number == 0) {
+                    block +="<li>Address: "+posts[index].street+", "+posts[index].zipcode+"</li>";
+                  } else {
+                    block +="<li>Address: "+posts[index].number +","+ posts[index].street+", " + posts[index].city +", "+posts[index].zipcode+"</li>";
+                  }
+                block +="</ul>";
+                block += "<a class='btn btn-primary href=/event/" +posts[index].id+ "> More details </a>";
+              block +="</div>";            
+            block +="</div>";
           block +="</div>";
-        block +="</div>";
-        $(".container-occasions").append(block);
+          $(".container-occasions").append(block);
         // console.log(posts[index].name);
 
         /*

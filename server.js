e0a3248f08c222a266f5a81598eaac8b8dc117e1
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-06-14 22:04:22 -0700

    changing server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var loginRoutes = require("./controllers/loginControllers.js");
 
 app.use("/login", loginRoutes);
 
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
